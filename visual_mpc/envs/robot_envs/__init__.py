b720210a2b8d251b47d64b3f12f2e07f7452f51f
Karl Schmeckpeper <karls@seas.upenn.edu>
2019-06-03 17:50:16 -0400

    Added baxter controller as an option

--- visual_mpc/envs/robot_envs/__init__.py
+++ visual_mpc/envs/robot_envs/__init__.py
@@ -6,5 +6,8 @@ def get_controller_class(robot_type):
     if robot_type == 'sawyer':
         from .sawyer.sawyer_impedance import SawyerImpedanceController
         return SawyerImpedanceController
+    elif robot_type == 'baxter':
+        from .baxter.baxter_impedance import BaxterImpedanceController
+        return BaxterImpedanceController
     else:
         raise NotImplementedError
