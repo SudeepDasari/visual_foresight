d647e3f44c047d2de12b73f9f2a67108d8688086
Stephen <stephentian@berkeley.edu>
2019-10-14 18:35:19 -0700

    Update context action sampler

--- visual_mpc/policy/inverse_models/inverse_model_base_controller.py
+++ visual_mpc/policy/inverse_models/inverse_model_base_controller.py
@@ -2,7 +2,6 @@ import numpy as np
 from visual_mpc.utils.logger import Logger
 from visual_mpc.policy.policy import Policy
 from robonet.inverse_model.testing.action_inference_interface import ActionInferenceInterface
-from .samplers import GaussianCEMSampler
 
 class InvModelBaseController(Policy):
     """
@@ -28,7 +27,6 @@ class InvModelBaseController(Policy):
         #action dimensions:
         self._adim = self.agentparams['adim']
         self._sdim = self.agentparams['sdim']
-        #predictor_hparams = {'load_T':self._hp.load_T}
         predictor_hparams = {}
         self.predictor = self._hp.predictor_class(self._hp.model_params_path, predictor_hparams, n_gpus=ngpu, first_gpu=gpu_id)
         self.predictor.restore()
@@ -48,8 +46,9 @@ class InvModelBaseController(Policy):
             'load_T':7,
             'num_context': 2,
             'replan_every': 2,
-            'context_action_weight': [0.5, 0.5, 0.05, 1],
-            'sampler': GaussianCEMSampler,
+            'context_action_weight': [1, 1, 1, 1],
+            'initial_action_low': [-0.025, -0.025, -0.025, 0],
+            'initial_action_high': [0.025, 0.025, 0.025, 0],
         }
 
         parent_params = super(InvModelBaseController, self)._default_hparams()
@@ -64,26 +63,31 @@ class InvModelBaseController(Policy):
         self.context_actions = [None] * self._hp.num_context
         self.context_frames = [None] * self._hp.num_context
 
+    def _sample_initial_action(self):
+        return np.random.uniform(self._hp.initial_action_low, self._hp.initial_action_high)
+
     def act(self, t=None, i_tr=None, images=None, goal_image=None):
 
         if t < self._hp.num_context:
-            initial_sampler = self._hp.sampler(self._hp, self._adim, self._sdim)
-            action = initial_sampler.sample_initial_actions(t, 1, state[-1])[0, 0] * self._hp.context_action_weight
-
+            action = self._sample_initial_action() * self._hp.context_action_weight
         elif t >= self._hp.num_context:
-            if ((t - self._hp.num_context) % self._hp.replan_every == 0):
+            if (t - self._hp.num_context) % self._hp.replan_every == 0:
+                # Perform replanning here.
                 float_ctx = [frame[None, None] for frame in self.context_frames] 
                 prepped_ctx_im = np.concatenate(float_ctx, axis=1)
                 prepped_ctx_act = np.array(self.context_actions)[None] 
                 self.actions = self.predictor(convert_to_float(images[-1,0]), goal_image[-1, 0],
                                               prepped_ctx_act, prepped_ctx_im)  # select last-image and 0-th camera
+                # action_counter represents the amount of time since the last replan
                 self.action_counter = 0
             print('t {} action counter {}'.format(t, self.action_counter))
+            assert self.actions.shape[1] > self.action_counter, \
+                'Tried to take action {} of plan containing {}. ' \
+                'Maybe re-planning is not occurring often enough?'.format(self.action_counter, self.actions.shape[1])
+
             action = self.actions[0, self.action_counter]
             self.action_counter += 1
-        else:
-            action = np.zeros(self._adim)
-       
+
         print('action ', action)
         new_context_image = convert_to_float(np.copy(images[-1, 0]))
         self.update_context(new_context_image, action)
@@ -93,6 +97,7 @@ class InvModelBaseController(Policy):
         self.context_frames.append(new_image)
         self.context_actions.append(new_action)
         if len(self.context_frames) > self._hp.num_context:
+            # Maintain newest num_context context frames & actions
             self.context_frames.pop(0)
             self.context_actions.pop(0)
         
